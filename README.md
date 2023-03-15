39efc7b75c5cd752881200e90b72c99d5e8f367f
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-15 08:45:25 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# flutter_fasting
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/com/example/flutter_fasting/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.flutter_fasting
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
