7edb0f545dd7412d14b4ded4f7ead993c294e08b
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-21 21:07:18 -0500

    add more formatting to fasts list and chart container

--- lib/main.dart
+++ lib/main.dart
@@ -22,11 +22,11 @@ class MyHomePage extends StatelessWidget {
   // final formatter = new DateFormat('MM/dd/yyyy hh:mm');
   final List<Fast> fasts = [
     Fast(
-      id: '0',
+      id: '1',
       startDateTime: now,
       endDateTime: later,
     ),
-    Fast(id: '1', startDateTime: yesterday, endDateTime: yesterdayLater),
+    Fast(id: '2', startDateTime: yesterday, endDateTime: yesterdayLater),
   ];
 
   @override
@@ -39,10 +39,13 @@ class MyHomePage extends StatelessWidget {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: <Widget>[
           Container(
+            margin: EdgeInsets.all(10),
+            padding: EdgeInsets.all(20),
+            alignment: Alignment.center,
             width: double.infinity,
             child: Card(
               color: Colors.purple,
-              child: Text('CHART!'),
+              child: Text('CHART!', style: TextStyle(fontSize: 25)),
               elevation: 5,
             ),
           ),
@@ -52,15 +55,33 @@ class MyHomePage extends StatelessWidget {
                   child: Row(
                 children: <Widget>[
                   Container(
-                    child: Text(fast.hoursFasted.toString()),
+                    margin: EdgeInsets.symmetric(
+                      vertical: 10,
+                      horizontal: 25,
+                    ),
+                    decoration: BoxDecoration(
+                        border: Border.all(
+                      color: Colors.purple,
+                      width: 2,
+                    )),
+                    padding: EdgeInsets.all(10),
+                    child: Text(fast.hoursFasted.toString() + ' Hrs',
+                        style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 20,
+                            color: Colors.purple)),
                   ),
                   Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: <Widget>[
-                      Text(fast.id),
-                      Text('From: ' +
-                          fast.startDateTime.toString() +
-                          ' to ' +
-                          fast.endDateTime.toString()),
+                      Text('Fast #: ${fast.id}',
+                          style: TextStyle(
+                              fontSize: 18,
+                              fontWeight: FontWeight.bold,
+                              color: Colors.grey)),
+                      Text('${fast.startDateTime.toString()} -' // +
+                          // fast.endDateTime.toString()
+                          ),
                     ],
                   ),
                 ],
