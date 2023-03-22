9e789668df48b3443f003f5e15624fe1f2a711f3
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-22 11:24:58 -0500

    extra new fast (fast entry) widget)

---
+++ lib/widgets/new_fast.dart
@@ -0,0 +1,35 @@
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+import 'package:flutter/material.dart';
+
+class NewFast extends StatelessWidget {
+  final idController = TextEditingController();
+  final hoursController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      elevation: 5,
+      child: Container(
+        padding: EdgeInsets.all(10),
+        child: Column(
+          children: <Widget>[
+            TextField(
+              decoration: InputDecoration(labelText: 'Fast Number'),
+              controller: idController,
+            ),
+            TextField(
+              decoration: InputDecoration(labelText: 'Hours'),
+              controller: hoursController,
+            ),
+            TextButton(
+              child: Text('Add Fast'),
+              onPressed: () => print(
+                  'submitted: ${idController.text} and ${hoursController.text}'),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
