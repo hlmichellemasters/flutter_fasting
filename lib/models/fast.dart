df4225a1602cb609a351fa37425b6ebd3b852b5a
hlmichellemasters <hlmichellemasters@gmail.com>
2023-03-23 11:02:43 -0500

    remove id from fast model

--- lib/models/fast.dart
+++ lib/models/fast.dart
@@ -1,14 +1,10 @@
 final MINUTES_IN_AN_HOUR = 60;
 
 class Fast {
-  final String id;
   final DateTime startDateTime;
   final DateTime endDateTime;
 
-  Fast(
-      {required this.id,
-      required this.startDateTime,
-      required this.endDateTime});
+  Fast({required this.startDateTime, required this.endDateTime});
 
   double get hoursFasted =>
       (this.endDateTime.difference(this.startDateTime).inMinutes) /
